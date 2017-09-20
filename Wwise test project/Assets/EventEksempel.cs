c2b2440814137aaa5072e52ea8ccd2c5edeae60c
Unknown <esbennj@hotmail.com>
2017-09-20 17:49:39 +0200

    Made a project

---
+++ Wwise test project/Assets/EventEksempel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventEksempel : MonoBehaviour {
+
+	Vector3 startpos; 
+
+
+	void Start () {
+		startpos = transform.position; 
+
+		// Event1: "Ambience" sættes igang ved start 
+		AkSoundEngine.PostEvent ("Ambience", gameObject); 
+
+	}
+	
+
+	void Update () {
+		transform.position = new Vector3 (transform.position.x + (Input.GetAxis ("Horizontal") / 10), transform.position.y, transform.position.z); 
+
+		float xpos = startpos.x - transform.position.x;
+
+
+		// Event2: "PressSpace" kaldes ved tryk af knap 
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			AkSoundEngine.PostEvent ("PressSpace", gameObject); 
+			transform.localScale = new Vector3 (Random.Range (0.2f, 1.5f), Random.Range (0.2f, 1.5f), Random.Range (0.2f, 1.5f)); 
+		}
+
+		// GameSync1 (Switch): "LeftRight" skifter mellem PressSpace-filer ud fra binær x 
+		// GameSync2 (State): "LeftRight" fader mellem Ambience-filer ud fra binær x
+		if (transform.position.x <= startpos.x) {
+			AkSoundEngine.SetSwitch ("LeftRight", "Left", gameObject); 
+			AkSoundEngine.SetState ("LeftRight", "Left"); 
+		
+		} else if (transform.position.x > startpos.x) { 
+			AkSoundEngine.SetSwitch ("LeftRight", "Right", gameObject); 
+			AkSoundEngine.SetState ("LeftRight", "Right"); 
+
+		}
+
+		// GameSync3 (RTPC): "XPosition" lader x som talværdi styre parametre for PressSpace & Ambience 
+		AkSoundEngine.SetRTPCValue ("XPosition", xpos);
+
+
+	}
+}
---
+++ Wwise test project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID PRESSSPACE = 2698625392U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace LEFTRIGHT
+        {
+            static const AkUniqueID GROUP = 2583390356U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LEFT = 4109362U;
+                static const AkUniqueID RIGHT = 3893817417U;
+            } // namespace STATE
+        } // namespace LEFTRIGHT
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace LEFTRIGHT
+        {
+            static const AkUniqueID GROUP = 2583390356U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LEFT = 4109362U;
+                static const AkUniqueID RIGHT = 3893817417U;
+            } // namespace SWITCH
+        } // namespace LEFTRIGHT
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID XPOSITION = 779132718U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID INTEGRATIONSHIT = 3416573779U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
